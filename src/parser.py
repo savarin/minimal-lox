69d5d94ee46843f7cb5e83735e46a44b523f5aa1
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-25 03:02:00 -0800

    binary - parser

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+from expr import Operator
 from scanner import TokenType
 import expr
 import scanner
@@ -11,6 +12,10 @@ def parse(tokens: List[scanner.Token]) -> statem.Statem:
 
     statement, _ = expression(tokens, counter)
     return statement
+
+
+def peek(tokens: List[scanner.Token], counter: int, token_type: TokenType) -> bool:
+    return tokens[counter].token_type == token_type
 
 
 def expect(
@@ -28,13 +33,52 @@ def expect(
 
 
 def expression(tokens: List[scanner.Token], counter: int) -> Tuple[statem.Statem, int]:
-    value, counter = integer(tokens, counter)
+    value, counter = term(tokens, counter)
     _, counter = expect(tokens, counter, TokenType.SEMICOLON)
 
     return statem.Expression(value), counter
 
 
-def integer(tokens: List[scanner.Token], counter: int) -> Tuple[expr.Expr, int]:
+def term(tokens: List[scanner.Token], counter: int) -> Tuple[expr.Expr, int]:
+    left, counter = factor(tokens, counter)
+
+    while True:
+        if peek(tokens, counter, TokenType.PLUS):
+            token, counter = expect(tokens, counter, TokenType.PLUS)
+            right, counter = factor(tokens, counter)
+
+            left = expr.Numeric(Operator.PLUS, left, right)
+            continue
+
+        elif peek(tokens, counter, TokenType.MINUS):
+            token, counter = expect(tokens, counter, TokenType.MINUS)
+            right, counter = factor(tokens, counter)
+
+            left = expr.Numeric(Operator.MINUS, left, right)
+            continue
+
+        break
+
+    return left, counter
+
+
+def factor(tokens: List[scanner.Token], counter: int) -> Tuple[expr.Expr, int]:
+    left, counter = primary(tokens, counter)
+
+    while True:
+        if peek(tokens, counter, TokenType.TIMES):
+            token, counter = expect(tokens, counter, TokenType.TIMES)
+            right, counter = primary(tokens, counter)
+
+            left = expr.Numeric(Operator.TIMES, left, right)
+            continue
+
+        break
+
+    return left, counter
+
+
+def primary(tokens: List[scanner.Token], counter: int) -> Tuple[expr.Expr, int]:
     token, counter = expect(tokens, counter, TokenType.INTEGER)
 
     return expr.Integer(token.value), counter
--- src/test_parser.py
+++ src/test_parser.py
@@ -1,3 +1,4 @@
+from expr import Operator
 from scanner import TokenType
 import expr
 import scanner
@@ -13,3 +14,15 @@ def test_parse() -> None:
     ]
 
     assert parser.parse(tokens) == statem.Expression(expr.Integer("1"))
+
+    tokens = [
+        scanner.Token(TokenType.INTEGER, "2", 1),
+        scanner.Token(TokenType.PLUS, "+", 1),
+        scanner.Token(TokenType.INTEGER, "3", 1),
+        scanner.Token(TokenType.SEMICOLON, ";", 1),
+        scanner.Token(TokenType.EOF, "EOF", 1),
+    ]
+
+    assert parser.parse(tokens) == statem.Expression(
+        expr.Numeric(Operator.PLUS, expr.Integer("2"), expr.Integer("3"))
+    )
