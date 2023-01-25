ae86ff10c5e4b0b1b8b6e93def26e4d53694b9ec
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-25 02:31:11 -0800

    binary - interpreter

--- src/interpreter.py
+++ src/interpreter.py
@@ -16,5 +16,11 @@ def evaluate(expression: expr.Expr) -> int:
         case expr.Integer(value):
             return int(value)
 
+        case expr.Numeric(operator, left, right):
+            left_eval = evaluate(left)
+            right_eval = evaluate(right)
+
+            return eval(f"{left_eval} {operator.value} {right_eval}")
+
         case _:
             raise Exception(f"Exhaustive switch error on expression {str(expression)}.")
--- src/test_interpreter.py
+++ src/test_interpreter.py
@@ -1,3 +1,4 @@
+from expr import Operator
 import expr
 import interpreter
 import statem
@@ -5,3 +6,12 @@ import statem
 
 def test_interpret() -> None:
     assert interpreter.interpret(statem.Expression(expr.Integer("1"))) == 1
+
+    assert (
+        interpreter.interpret(
+            statem.Expression(
+                expr.Numeric(Operator.PLUS, expr.Integer("2"), expr.Integer("3"))
+            )
+        )
+        == 5
+    )
